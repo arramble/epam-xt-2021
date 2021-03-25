70825253d730bc59d0a2b3ad9232f42c6c5a8474
Alexander Gerasimov <arramble@gmail.com>
2021-03-26 00:44:00 +0300

    Add task 3.1.2

---
+++ Task 3/Task 3.1/Task 3.1.2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace Task3_1_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            string input = InputText();
+            string[] words = SplitText(input);
+            DisplayStatistics(words);
+
+            Console.ReadKey(true);
+        }
+
+        static string InputText()
+        {
+            Console.Write("Введите текст: ");
+
+            return Console.ReadLine();
+        }
+
+        static string[] SplitText(string text)
+        {
+            char[] separators = new char[] { ' ', ':', '.', ',', ';', '!', '?', '(', ')', '-', '"' };
+
+            return text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        static void DisplayStatistics(string[] words)
+        {
+            var wordGroups = words.GroupBy(w => w.ToLower())
+                                  .Select(g => new { Word = g.Key, Count = g.Count() })
+                                  .OrderByDescending(g => g.Count);
+
+            Console.WriteLine();
+            Console.WriteLine("Частота использования слов в тексте:");
+
+            foreach (var group in wordGroups)
+            {
+                Console.WriteLine($"{group.Word}: {group.Count}");
+            }
+        }
+    }
+}
