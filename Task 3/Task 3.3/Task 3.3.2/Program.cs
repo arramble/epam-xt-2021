e947595d423107fba7012044735895079e31b3f8
Alexander Gerasimov <arramble@gmail.com>
2021-04-05 01:59:32 +0400

    Add task 3.3.2

---
+++ Task 3/Task 3.3/Task 3.3.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task3_3_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            TestSuperString();
+
+            Console.ReadKey(true);
+        }
+
+        static void TestSuperString()
+        {
+            Console.Write("Введите слово: ");
+
+            string input = Console.ReadLine();
+
+            Console.WriteLine("Язык слова: " + input.GetLanguage());
+        }
+    }
+}
---
+++ Task 3/Task 3.3/Task 3.3.2/StringExtension.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace Task3_3_2
+{
+    public enum Language
+    {
+        Mixed,
+        Russian,
+        English,
+        Number
+    }
+
+    public static class StringExtension
+    {
+        public static Language GetLanguage(this string word)
+        {
+            char[] chars = word.ToCharArray();
+
+            if (chars.All(c => Char.IsDigit(c)))
+            {
+                return Language.Number;
+            }
+            else if (chars.All(c => (c >= 'а' && c <= 'я') || (c >= 'А' && c <= 'Я')))
+            {
+                return Language.Russian;
+            }
+            else if (chars.All(c => (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')))
+            {
+                return Language.English;
+            }
+            else
+            {
+                return Language.Mixed;
+            }
+        }
+    }
+}
